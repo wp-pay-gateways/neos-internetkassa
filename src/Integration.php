b5adcbb1bb0941bbafaf212c48d3dceec82ac0a6
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:04:06 +0100

    Extend from Ogone library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_NEOS_InternetKassa_Integration {
+class Pronamic_WP_Pay_Gateways_NEOS_InternetKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id         = 'neos-internetkassa';
 		$this->name       = 'NEOS - Internet Kassa';
 		$this->provider   = 'neos';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Gateway';
 	}
 }
