953a7647319e71ca909ab24a9b1f1f3c72ac943a
Remco Tolsma <info@remcotolsma.nl>
2016-01-04 12:18:12 +0100

    Added tests.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: NEOS - Internet Kassa - Integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_NEOS_InternetKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id         = 'neos-internetkassa';
---
+++ tests/IntegrationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: NEOS - Internet Kassa - Integration test
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_NEOS_InternetKassa_IntegrationTest extends PHPUnit_Framework_TestCase {
+	public function test_config() {
+		$integration = new Pronamic_WP_Pay_Gateways_NEOS_InternetKassa_Integration();
+
+	}
+}
